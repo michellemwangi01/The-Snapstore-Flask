9f242f532cb35214bdb0cf0c8e38acffe3dd0d46
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-10-04 06:49:25 +0300

    modified the footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -12,6 +12,10 @@ const Footer = () => {
             <ul className="list-unstyled list-inline text-md-right mb-0">
               <li className="list-inline-item"><a href="/about" className="text-light">About</a></li>
               <li className="list-inline-item"><a href="/contact" className="text-light">Contact</a></li>
+              <li className="list-inline-item"><a href="https://www.facebook.com" className="text-light">Facebook</a></li>
+              <li className="list-inline-item"><a href="https://www.twitter.com" className="text-light">Twitter</a></li>
+              <li className="list-inline-item"><a href="https://www.instagram.com" className="text-light">Instagram</a></li>
+              <li className="list-inline-item"><a href="https://www.linkedin.com" className="text-light">LinkedIn</a></li>
             </ul>
           </div>
         </div>
