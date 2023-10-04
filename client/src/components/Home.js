ef302a9f7ee073501de71e5444e6ed39fa5150f9
gilbert <gilbert.bageni@student.moringaschool.com>
2023-10-04 23:28:48 +0300

    Your commit message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from 'react';
 function Home() {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [likes, setLikes] = useState({}); // Store liked photos
-  const [cart, setCart] = useState([]); // Store items in the cart
+  const [likes, setLikes] = useState({});
+  const [cart, setCart] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [photosPerPage] = useState(6); // Set the number of photos per page
 
   useEffect(() => {
     // Replace 'YOUR_API_URL' with the actual URL of your API endpoint for photos
@@ -24,37 +26,81 @@ function Home() {
     return <p>Loading...</p>;
   }
 
-  // Function to handle liking or unliking a photo
   const toggleLike = (photoId) => {
     const updatedLikes = { ...likes };
     updatedLikes[photoId] = !updatedLikes[photoId];
     setLikes(updatedLikes);
   };
 
-  // Function to handle adding a photo to the cart
   const addToCart = (photo) => {
     setCart([...cart, photo]);
   };
 
+  // Calculate the indexes for the photos to display on the current page
+  const indexOfLastPhoto = currentPage * photosPerPage;
+  const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage;
+  const currentPhotos = photos.slice(indexOfFirstPhoto, indexOfLastPhoto);
+
+  // Function to handle page navigation
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(photos.length / photosPerPage);
+
   return (
     <div>
-      <h1>Welcome to SnapStore</h1>
-      <div className="photo-list">
-        {photos.map((photo) => (
-          <div key={photo.id} className="photo-item">
-            <img src={photo.image} alt={photo.name} />
-            <h2>{photo.name}</h2>
-            <p>{photo.description}</p>
-            <p>Price: ${photo.price}</p>
-            <button onClick={() => toggleLike(photo.id)}>
-              {likes[photo.id] ? 'Unlike' : 'Like'}
-            </button>
-            <button onClick={() => addToCart(photo)}>
-              Add to Cart
-            </button>
+      <h1 className="my-4">Welcome to SnapStore</h1>
+      <div className="row">
+        {currentPhotos.map((photo) => (
+          <div key={photo.id} className="col-md-4 mb-4">
+            <div className="card card-sm"> {/* Add card-sm class to make cards smaller */}
+              <img src={photo.image} alt={photo.name} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{photo.name}</h5>
+                <p className="card-text">{photo.description}</p>
+                <p className="card-text">Price: ${photo.price}</p>
+                <button className="btn btn-primary" onClick={() => toggleLike(photo.id)}>
+                  {likes[photo.id] ? 'Unlike' : 'Like'}
+                </button>
+                <button className="btn btn-success ml-2" onClick={() => addToCart(photo)}>
+                  Add to Cart
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+      <nav className="mt-4" aria-label="Page navigation">
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+          </li>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+              <button className="page-link" onClick={() => paginate(index + 1)}>
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
       <div className="cart">
         <h2>Cart</h2>
         <ul>
@@ -69,4 +115,4 @@ function Home() {
   );
 }
 
-export default Home; 
+export default Home;
