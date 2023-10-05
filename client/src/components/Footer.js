43b9424f0038c4abe3aa07f8ed44ed483e9c1998
gilbert <gilbert.bageni@student.moringaschool.com>
2023-10-05 07:56:00 +0300

    updated home.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
     <footer className="bg-dark text-light py-4">
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-lg-3 col-md-6 mb-4">
             <h5>About Us</h5>
-            <p> Snapstore is your premier destination for high-quality photography. Our team of photographers and artists is passionate about delivering stunning visuals that resonate with your audience.</p>
+            <p>Snapstore is your premier destination for high-quality photography. Our team of photographers and artists is passionate about delivering stunning visuals that resonate with your audience.</p>
             <a href="/about" className="text-light">Learn More</a>
           </div>
-          <div className="col-md-4">
+          <div className="col-lg-3 col-md-6 mb-4">
             <h5>Contact Us</h5>
             <ul className="list-unstyled">
               <li>Email: <a href="mailto:moringaGroup2@snapstore.com" className="text-light">moringaGroup2@snapstore.com</a></li>
@@ -19,19 +19,32 @@ const Footer = () => {
               <li><a href="/contact" className="text-light">Contact Form</a></li>
             </ul>
           </div>
-          <div className="col-md-4">
+          <div className="col-lg-3 col-md-6 mb-4">
             <h5>Connect with Us</h5>
             <ul className="list-unstyled list-inline">
-              <li className="list-inline-item"><a href="https://www.facebook.com" className="text-light"><FaFacebook /></a></li>
-              <li className="list-inline-item"><a href="https://www.twitter.com" className="text-light"><FaTwitter /></a></li>
-              <li className="list-inline-item"><a href="https://www.instagram.com" className="text-light"><FaInstagram /></a></li>
-              <li className="list-inline-item"><a href="https://www.linkedin.com" className="text-light"><FaLinkedin /></a></li>
+              <li className="list-inline-item">
+                <a href="https://www.facebook.com" className="text-light" title="Facebook">
+                  <FaFacebook className="social-icon" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.twitter.com" className="text-light" title="Twitter">
+                  <FaTwitter className="social-icon" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.instagram.com" className="text-light" title="Instagram">
+                  <FaInstagram className="social-icon" />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.linkedin.com" className="text-light" title="LinkedIn">
+                  <FaLinkedin className="social-icon" />
+                </a>
+              </li>
             </ul>
           </div>
-        </div>
-        <hr className="bg-secondary my-4" />
-        <div className="row">
-          <div className="col-md-4">
+          <div className="col-lg-3 col-md-6 mb-4">
             <h5>Products</h5>
             <ul className="list-unstyled">
               <li><a href="/products" className="text-light">All Products</a></li>
@@ -39,7 +52,10 @@ const Footer = () => {
               <li><a href="/specials" className="text-light">Special Offers</a></li>
             </ul>
           </div>
-          <div className="col-md-4">
+        </div>
+        <hr className="bg-secondary my-4" />
+        <div className="row">
+          <div className="col-md-6">
             <h5>Customer Support</h5>
             <ul className="list-unstyled">
               <li><a href="/faq" className="text-light">FAQs</a></li>
@@ -47,7 +63,7 @@ const Footer = () => {
               <li><a href="/returns" className="text-light">Return Policy</a></li>
             </ul>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-6">
             <h5>Subscribe to Newsletter</h5>
             <p>Stay updated with our latest products and offers.</p>
             <form>
@@ -58,13 +74,9 @@ const Footer = () => {
             </form>
           </div>
         </div>
-      </div>
-      <div className="container-fluid bg-danger mt-4">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <p className="mb-0">&copy; {new Date().getFullYear()} SNAPSTORE. All rights reserved.</p>
-            </div>
+        <div className="row">
+          <div className="col-md-12 text-center mt-4">
+            <p className="mb-0">&copy; {new Date().getFullYear()} SNAPSTORE. All rights reserved.</p>
           </div>
         </div>
       </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Cart from './Cart'; // Import the Cart component
 
 function Home() {
   const [photos, setPhotos] = useState([]);
@@ -50,66 +51,63 @@ function Home() {
   const totalPages = Math.ceil(photos.length / photosPerPage);
 
   return (
-    <div>
+    <div className="container">
       <h1 className="my-4">Welcome to SnapStore</h1>
       <div className="row">
-        {currentPhotos.map((photo) => (
-          <div key={photo.id} className="col-md-4 mb-4">
-            <div className="card card-sm"> {/* Add card-sm class to make cards smaller */}
-              <img src={photo.image} alt={photo.name} className="card-img-top" />
-              <div className="card-body">
-                <h5 className="card-title">{photo.name}</h5>
-                <p className="card-text">{photo.description}</p>
-                <p className="card-text">Price: ${photo.price}</p>
-                <button className="btn btn-primary" onClick={() => toggleLike(photo.id)}>
-                  {likes[photo.id] ? 'Unlike' : 'Like'}
-                </button>
-                <button className="btn btn-success ml-2" onClick={() => addToCart(photo)}>
-                  Add to Cart
-                </button>
+        <div className="col-md-9"> {/* Content column */}
+          <div className="row">
+            {currentPhotos.map((photo) => (
+              <div key={photo.id} className="col-md-4 mb-4">
+                <div className="card card-sm"> {/* Add card-sm class to make cards smaller */}
+                  <img src={photo.image} alt={photo.name} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title">{photo.name}</h5>
+                    <p className="card-text">{photo.description}</p>
+                    <p className="card-text">Price: ${photo.price}</p>
+                    <button className={`btn custom-love-button ${likes[photo.id] ? 'text-danger' : ''}`} onClick={() => toggleLike(photo.id)}>
+                      {likes[photo.id] ? '❤️ Love' : '❤️ Like'}
+                    </button>
+                    <button className="btn btn-warning ml-2" onClick={() => addToCart(photo)}>
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <nav className="mt-4" aria-label="Page navigation">
-        <ul className="pagination justify-content-center">
-          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-            <button
-              className="page-link"
-              onClick={() => paginate(currentPage - 1)}
-              disabled={currentPage === 1}
-            >
-              Previous
-            </button>
-          </li>
-          {Array.from({ length: totalPages }, (_, index) => (
-            <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-              <button className="page-link" onClick={() => paginate(index + 1)}>
-                {index + 1}
-              </button>
-            </li>
-          ))}
-          <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-            <button
-              className="page-link"
-              onClick={() => paginate(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            >
-              Next
-            </button>
-          </li>
-        </ul>
-      </nav>
-      <div className="cart">
-        <h2>Cart</h2>
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              {item.name} - Price: ${item.price}
-            </li>
-          ))}
-        </ul>
+          <nav className="mt-4" aria-label="Page navigation">
+            <ul className="pagination justify-content-center">
+              <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                <button
+                  className="page-link"
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </button>
+              </li>
+              {Array.from({ length: totalPages }, (_, index) => (
+                <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                  <button className="page-link" onClick={() => paginate(index + 1)}>
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+              <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                <button
+                  className="page-link"
+                  onClick={() => paginate(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                >
+                  Next
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-md-3"> {/* Cart column */}
+          <Cart cartItems={cart} />
+        </div>
       </div>
     </div>
   );
