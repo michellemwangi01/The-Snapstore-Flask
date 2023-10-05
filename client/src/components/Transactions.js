f34720952d8477f5d5bafa2fd12c880a987af055
user <mutukukennedy34@gmail.com>
2023-10-05 18:07:59 +0300

    table

--- client/src/components/Transactions.js
+++ client/src/components/Transactions.js
@@ -1,19 +1,55 @@
-
-import React , {useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import ConfirmDeleteDialog from './Confirmdelet';
 
 function Transactions() {
+  const [transactions, setTransactions] = useState([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
 
-  const [transactions , setTransaction] = useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://127.0.0.1:5555/api/transactions")
-    .then((resp) => resp.json())
-    .then((data)=>{
-    console.log(data);
-      setTransaction(data)
-    })
-  } , [])
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setTransactions(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
+  const handleDelete = async (transactionId) => {
+    // Open the confirmation dialog and store the selected item ID
+    setIsDialogOpen(true);
+    setSelectedItemId(transactionId);
+  };
+
+  const handleCancel = () => {
+    // Close the confirmation dialog
+    setIsDialogOpen(false);
+    setSelectedItemId(null);
+  };
+
+  const handleConfirm = async () => {
+    try {
+      // Send a DELETE request to the API to delete the resource
+      await fetch(`http://127.0.0.1:5555/api/transactions/${selectedItemId}`, {
+        method: 'DELETE',
+      });
+        // Update the local state by removing the deleted resource
+        setTransactions((prevTransactions) =>
+        prevTransactions.filter((transaction) => transaction.id !== selectedItemId)
+      );
+
+      // Close the confirmation dialog
+      setIsDialogOpen(false);
+      setSelectedItemId(null);
+    } catch (error) {
+      console.error('Error deleting resource:', error);
+    }
+  };
   return (
     <div className="tablediv">
       <table id="transaction">
@@ -22,32 +58,38 @@ function Transactions() {
             <th>Id</th>
             <th>Quantity</th>
             <th>Amount</th>
-            <th>Photoid</th>
-            <th> Userid</th>
-               <th></th>
+            <th>PhotoName</th>
+            <th>Username</th>
+            <th></th>
           </tr>
-        
-          {transactions.map((item) => (
-            
-            <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.quantity}</td>
-            <td>{item.amount}</td>
-            <td>{item.photo.id}</td>
-            <td>{item.user.id}</td>
 
-            <td>
-              
-            <button> Delete </button>  
-          
-             </td>
-          </tr>
-   
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  {transactions.map((item, index) => (
+    <tr key={item.id}>
+      <td>{index + 1}</td>
+      <td>{item.quantity}</td>
+      <td> KSH {item.amount}</td>
+      <td>{item.photo.name}</td>
+      <td>{item.user.username}</td>
+      <td>
+        <FontAwesomeIcon
+          color='red'
+          id='myicon'
+          icon={faTrash}
+          onClick={() => handleDelete(item.id)}
+        />
+      </td>
+    </tr>
+  ))}
+</tbody>
+</table>
+
+<ConfirmDeleteDialog
+isOpen={isDialogOpen}
+onCancel={handleCancel}
+onConfirm={handleConfirm}
+/>
+</div>
+);
 }
 
 export default Transactions;
